5234db7f3894cde00a2e3dcf8d166f8cfdd94597
valentinggggg <garellovalentin@gmail.com>
2023-04-11 23:38:17 -0300

    confirmacion terminada

--- main.js
+++ main.js
@@ -2,16 +2,42 @@ $(document).ready(function(){});
 
 function subirForm(event) {
   event.preventDefault();
-  var errorname = $("#errorname").val();
-  var errorapellido = $("#errorapellido").val();
-  var errormail = $("#errormail").val(); 
-  var errortelefono = $("#errortelefono").val(); 
-  var errormensaje = $("#errormensaje").val();
+  let mistakes = [];
+  const nombre = $("#itname").val();
+  const apellido = $("#itapellido").val();
+  const mail = $("#itemail").val(); 
+  const telefono = $("#itTelefono").val(); 
+  const mensaje = $("#itmessage").val();
 
-  $("input").each(function(){
-    if ($(this).val === "" || $(this).val === " " || $(this).val === undefined) {
-      $(".errores").show
-    }
+  if (nombre == "" || nombre == " " || nombre == undefined )mistakes.push("Ingrese su Nombre");
 
+  if (apellido == "" || apellido == " " || apellido == undefined )mistakes.push("Ingrese su Apellido");
+        
+  if (mail == "" || mail == " " || mail == undefined )mistakes.push("Ingrese un Email ") ;
+
+  if (telefono == "" || telefono == " " || telefono == undefined )mistakes.push(" Ingrese un Telefono") ;
+
+  if (mensaje == "" || mensaje == " " || mensaje == undefined )mistakes.push("Ingrese un Mensaje") ;
+
+  console.log(mistakes);
+
+  if(mistakes.length > 0){
+    alert ("No se puede enviar el formulario");
+    $("#errores-cometidos").show();
+    mistakes.forEach((elemento)=>{
+      console.log(elemento);
+      $("#mistakesform").append(`<ul> ${elemento} </ul>`)
     })
+  }
+  else{
+    alert ("El formulario fue enviado")
+  }
+
   };
+
+
+  // var errorname = $("#errorname").val();
+  // var errorapellido = $("#errorapellido").val();
+  // var errormail = $("#errormail").val(); 
+  // var errortelefono = $("#errortelefono").val(); 
+  // var errormensaje = $("#errormensaje").val();
